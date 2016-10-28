70c7ad4debceea655e3f4f2d7fb066163f5ffae9
memkvia <cometss@mail.ru>
2016-10-29 01:09:30 +0300

    Загрузи и установи все необходимые программы Создай собственный Java проект и построй 5 различных Java-классов. Каждый из них должен содержать метод и печатать строку: "Мой первый класс на Java". Названия классов должны быть следующими: FirstClass, SecondClass, ThirdClass, Fourth, Fifth. Создай свой собственный публичный Git репозиторий и загрузи туда свое решение.

---
+++ src/FifthClass.java
@@ -0,0 +1,8 @@
+
+public class FifthClass {
+	public static void main(String[] args) {
+
+		System.out.println("Мой пятый класс на Java");
+	}
+
+}
---
+++ src/FourthClass.java
@@ -0,0 +1,8 @@
+
+public class FourthClass {
+	public static void main(String[] args) { 
+
+		System.out.println("Мой четвертый класс на Java");
+	}
+
+}
